92ebaf01639d82c4689544f0b2dc74349bd30d92
Andrés <andresgarvagnate@gmail.com>
2023-08-02 21:52:12 -0300

    Second Commit

--- src/components/Cart/Carrito.js
+++ src/components/Cart/Carrito.js
@@ -1,26 +1,3 @@
-/* import { useContext } from "react";
-import { dataContext } from "../Context/DataContext";
-import CartElements from "./CartElements";
-import CartTotal from "./CartTotal";
-import './Carrito.css';
-import Navbar from "../Navbar/Navbar";
-
-const Carrito = () =>{
-    const { cart } = useContext(dataContext);
-    
-    return cart.length > 0 ? (
-        <>
-        <Navbar />
-        <CartElements />
-        <CartTotal />
-        </>
-    ) : (
-    
-        <h2 className='cart-message-center'>El carrito esta vacío</h2>
-    )
-}
-
-export default Carrito; */
 import { useContext } from "react";
 import { dataContext } from "../Context/DataContext";
 import CartElements from "./CartElements";
